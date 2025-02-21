947de93027322fc6263e3f3c6526d2165d14d794
renalt <diasrenato@live.com>
2025-02-21 19:03:44 -0300

    The last commit is from like a couple weeks ago, don't beleive the lies... new class for creating obj from expenses.

---
+++ src/main/java/com/example/API/service/ObjectifierService.java
@@ -0,0 +1,63 @@
+package com.example.API.service;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.example.API.model.Expense;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class ObjectifierService {
+    private static final Pattern datePattern = Pattern.compile("\\b(\\d{2}/\\d{2}(?:/\\d{4})?)\\b");
+    private static final Pattern valuePattern = Pattern.compile("-?\\d{1,3}(?:\\.\\d{3})*,\\d{2}");
+
+    public void process (String inputText) {
+        String[] expensesDoc = splitByLine(inputText);
+        List<String> filteredExpenses = getFilterCharges(expensesDoc);
+        List<Expense> expensesObj = objectifyExtract(filteredExpenses);
+    }
+
+    public String[] splitByLine (String extractText) {
+        return extractText.split("\n");
+
+    }
+
+    public List<Expense> objectifyExtract (List<String> expenses) {
+        List<Expense> expensesObj = new ArrayList<>();
+
+        expenses.forEach(line -> {
+            Matcher dateMatcher = datePattern.matcher(line);
+            Matcher valueMatcher = valuePattern.matcher(line);
+
+            String date = dateMatcher.group(1); // Extract date
+            String valueStr = valueMatcher.group(1).replace(",", "."); // Convert to double format
+            double value = Double.parseDouble(valueStr);
+            String description = line.substring(dateMatcher.end(), valueMatcher.start()).trim();
+
+            mapToObj(expensesObj, value, description, date);
+        });
+        return expensesObj;
+    }
+    
+    public List<String> getFilterCharges (String[] inputText ) {
+        List<String> filteredCharges = new ArrayList<>();
+
+        for (int i = 0; i < inputText.length; i++) {
+            Matcher dateMatcher = datePattern.matcher(inputText[i]);
+            Matcher valueMatcher = valuePattern.matcher(inputText[i]);
+
+            if (dateMatcher.find() && valueMatcher.find()) {
+                //line has necessary items
+                filteredCharges.add(inputText[i]);
+            }
+        }
+        return filteredCharges;
+    }
+
+    public void mapToObj (List<Expense> expenses, Double amount, String transaction, String data){
+        expenses.add(new Expense(amount, transaction, data, ""));
+    }
+}
